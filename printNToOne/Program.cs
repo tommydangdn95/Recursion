76bd2843b3aa6cb3043a19371d5b5249baa36f23
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 09:06:33 +0700

    Print numbers from n to 1

---
+++ printNToOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace printNToOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNTo1(10);
+        }
+
+        private static void PrintNTo1(int n)
+        {
+            if (n > 0)
+            {
+                Console.Write($"{n} ");
+                PrintNTo1(n-1);
+            }
+        }
+    }
+}
