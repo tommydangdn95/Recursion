8bb0480d912b8f21d473379df5d107ea5b504b16
Tran Thuy <dtranthuy1995@gmail.com>
2020-04-04 09:12:17 +0700

    Sum The First N
    Write a program in C# Sharp to find the sum of first n

---
+++ SumTheFirstN/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumTheFirstN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = SumTheFirstN(10);
+            Console.WriteLine(sum);
+        }
+
+        private static int SumTheFirstN(int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            return n + SumTheFirstN(n - 1);
+        }
+    }
+}
